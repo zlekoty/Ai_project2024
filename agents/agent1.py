c0ca84fe92e88a9a757dc55cf64ca09f88de3652
Joanna Kołaczek <zle.koty@gmail.com>
2024-05-10 12:45:48 +0200

    agent1

---
+++ agents/agent1.py
@@ -0,0 +1,27 @@
+import tictactoe as ttt
+
+def agent1(s: ttt.State):
+    """
+        ---
+    """
+    #do we have to evaluate potential states (after performing actions) and choose the best???
+    
+    def feature1(s):
+        #ex check if possible to have 3 in row  
+        pass
+
+    def feature2(s):
+        #ex check if opponent can have 3 in row
+        pass
+
+    #..........
+
+    def evaluation():
+        
+        pass
+
+
+    def move():
+        board = 1
+        cell = 1
+        return ttt.Action(board, cell)
